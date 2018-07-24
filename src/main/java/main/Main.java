735c65da9b57811120e9758398dda83cd763f3da
yaegorko <yaegorko@gmail.com>
2018-07-24 16:34:55 +0400

    start commit

---
+++ src/main/java/main/Main.java
@@ -0,0 +1,24 @@
+package main;
+
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.servlet.ServletContextHandler;
+import org.eclipse.jetty.servlet.ServletHolder;
+import servlets.SignInServlet;
+import servlets.SignUpServlet;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+
+        ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
+        context.addServlet(new ServletHolder(new SignUpServlet()), "/signup");
+        context.addServlet(new ServletHolder(new SignInServlet()), "/signin");
+
+        Server server = new Server(8080);
+        server.setHandler(context);
+
+        server.start();
+        System.out.println("Server started");
+        server.join();
+    }
+}
